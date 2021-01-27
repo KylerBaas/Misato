31f67a172ed7b72b8f8ae9ed75472fb9b9bb6569
Kyler Baas <Kyler.Baas@gmail.com>
2021-01-26 23:41:23 -0500

    commit on master

---
+++ arrays.py
@@ -0,0 +1,5 @@
+haikuLine1Array = ["big", "small"]
+haikuLine2Array = ["large", "tiny"]
+haikuLine3Array = ["giant", "microscopic"]
+
+animeArray = [205, 101921, 30]
--- misato.py
+++ misato.py
@@ -9,17 +9,14 @@ import giphy_client
 from giphy_client.rest import ApiException
 from pprint import pprint
 
-import random
-from random import seed
-from random import randint
+from random import seed, randint
 
-import math
+from arrays import haikuLine1Array, haikuLine2Array, haikuLine3Array, animeArray
 
+import requests
 
 load_dotenv()
 
-#intents = discord.Intents.default()
-#intents.members = True
 
 # Discord Token
 discordToken = os.getenv('DISCORD_TOKEN')
@@ -33,7 +30,7 @@ apiInstance = giphy_client.DefaultApi()
 bot = commands.Bot(command_prefix='::')
 
 
-################# Event Listeners #################
+###################################################### EVENT LISTENERS ######################################################
 
 # Event listener for when bot has connected
 @bot.event
@@ -41,19 +38,16 @@ async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
 
+# Event listener for displaying gifs from giphy        
 @bot.command()
-async def yo(ctx):
-    await ctx.reply("Yo whattup")
-    
-        
-@bot.command()
-async def gif(ctx, query: str):
-    await ctx.send("Here's a random " + query + " gif for you")
+async def gif(ctx, query: str = None):
     try:
+        if query is None:
+            query = "nothing"
+
         apiResponse = apiInstance.gifs_search_get(giphyToken, query)
         dataSet = apiResponse.data
         dataLen = len(dataSet)
-        print(dataLen)
         range = 30
 
         # Check if the length of the dataset is shorter than the default search range of 30
@@ -62,40 +56,100 @@ async def gif(ctx, query: str):
 
         # Generate a seed and random number for selecting the gif
         seed(None)
-        randNum = randint(0, range)
-        # Display the gif selected from the data set
-        await ctx.send(dataSet[randNum].images.fixed_height.url)
+        randNum = randint(0, range - 1)
+        # Display the gif selected from the data set along with the message
+        await ctx.send(f'{ctx.author.mention}' " Here's a random " + query + " gif\n" f'{dataSet[randNum].images.fixed_height.url}')
 
     # Handle an ApiException related to the Giphy API
     except ApiException as apie:
         print("Exception when calling DefaultApi")
 
+
+
 @bot.command()
 async def haiku(ctx):
-    haikus = ["big", "samll"]
-    seed(None)
-    randNum = randint(0, 2)
-    ctx.send(haikus[randNum])
+
+    # Retrieve three lines for the haiku
+    haikuLine1 = generateRandNum(haikuLine1Array)
+    haikuLine2 = generateRandNum(haikuLine2Array)
+    haikuLine3 = generateRandNum(haikuLine3Array)
+
+    haiku = haikuLine1 + "\n" + haikuLine2 + "\n" + haikuLine3
+    await ctx.send(haiku)
+
 
 
 @bot.command()
-async def calc(ctx, num1: float, op, num2: float):
-    if num1 != None and op != None and num2 != None: 
-        await ctx.send("The answer is: ")
-        if op == '+':
-            await ctx.send(num1 + num2)
-        elif op == '-':
-            await ctx.send(num1 - num2)
-        elif op == '*':
-            await ctx.send(num1 * num2)
-        elif op == '^':
-            await ctx.send(num1 ** num2)
-        else:
-            await ctx.send("Invalid operator!")
-    else:
-        await ctx.send("Invalid format!")
+async def animeroulette(ctx):
+    seed(None)
+    randNum = randint(0, 120000)
+
+    received = queryAnime(randNum)
+
+    # If the original query failed, query for one of the anime
+    if received == False:
+        defaultAnime = generateRandNum(animeArray)
+        received = queryAnime(defaultAnime)
+
+    anime = received['data']['Media']
+
+    embed = discord.Embed(
+        title = anime['title']['romaji'], 
+        url = "https://anilist.co/anime/" + str(anime['id']),
+        description = "\"*Forrest Gump mama said, Life is like a box of chocolates (You never know what you're gonna get)*\"\n - Kanye West"
+    )
+    embed.set_image(url="https://i.redd.it/su8274uv8zh11.jpg")
+    await ctx.send(embed = embed)
 
 
+##################################################################################################################################################################
+
+
+###################################################### HELPER FUNCTIONS ######################################################
+
+def queryAnime(randNum):
+   # Query for 
+    query = '''
+    query ($id: Int) {
+        Media (id: $id, type: ANIME) {
+            id
+            title {
+                romaji
+                english
+            }
+        }
+    }'''
+
+    variables = {
+        'id': randNum
+    }
+
+    url = 'https://graphql.anilist.co'
+
+    try:
+        # Make an HTTP API request
+        response = requests.post(url, json = {'query': query, 'variables': variables})
+        
+        # Queried 
+        if response.status_code == 404:
+            return False
+    # Error in query, return with false
+    except requests.exceptions.RequestException as e:
+        return False
+
+    return response.json()
+
+
+# Generates a random number as an index in an array
+def generateRandNum(array):
+    size = len(array)
+    seed(None)
+    randNum = randint(0, size - 1)
+    return array[randNum]
+
+##################################################################################################################################################################
+
+# Main execution of bot
 try:
     bot.run(discordToken)
 except discord.errors.HTTPException and discord.errors.LoginFailure as e:
