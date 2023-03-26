73f40a7f080e1d82fe8fe58976044dcaf20bb69c
Kyler Baas <Kyler.Baas@gmail.com>
2023-03-26 00:02:51 -0400

    Added characters to roulette

--- modules/roulette/helper_functions.py
+++ modules/roulette/helper_functions.py
@@ -1,6 +1,7 @@
 from random import seed, randint
 import requests
 
+
 def generate_random_index(array):
     size = len(array)
     seed(None)
@@ -8,7 +9,7 @@ def generate_random_index(array):
     return array[randNum]
 
 
-def query_animemanga(rand_num, media_type: str):
+def query_anilist(rand_num, media_type: str):
     query = build_query(media_type)
     variables = {
         'id': rand_num
@@ -26,30 +27,80 @@ def query_animemanga(rand_num, media_type: str):
 
 
 def build_query(media_type: str):
-    episode_type = "episodes" if media_type == "ANIME" else "chapters"
     query_params = "$id: Int"
-    media_params = "id: $id, type: {type}, isAdult: false".format(type=media_type)
-    field_params = '''
-        id
-        title {{
-            romaji
-            english
-        }}
-        coverImage {{
-            large
-        }}
-        description
-        averageScore
-        {episode_type}
-        genres
-    '''.format(episode_type=episode_type)
+
+    if (media_type == "ANIME" or media_type == "MANGA"):
+        episode_type = "episodes" if media_type == "ANIME" else "chapters"
+        type_params = "Media(id: $id, type: {media_type}, isAdult: false)".format(
+            media_type=media_type)
+        field_params = '''
+            id
+            title {{
+                romaji
+                english
+            }}
+            coverImage {{
+                large
+            }}
+            description
+            averageScore
+            {episode_type}
+            genres
+        '''.format(episode_type=episode_type)
+    elif (media_type == "CHARACTER"):
+        type_params = "Character(id: $id)"
+        field_params = '''
+            id
+            name {{
+                full
+            }}
+            age
+            image {{
+                large
+            }}
+            dateOfBirth {{
+                year
+                month
+                day
+            }}
+            description
+            gender
+        '''.format()
 
     query = '''
         query({query_params}) {{
-            Media({media_params}){{
+            {type_params}{{
                 {field_params}
             }}
         }}
-    '''.format(query_params=query_params, media_params=media_params, field_params=field_params)
-
+    '''.format(query_params=query_params, type_params=type_params, field_params=field_params)
     return query
+
+
+def monthToString(month: int):
+    if (month is None):
+        return ""
+    if (month == 1):
+        return "January"
+    elif (month == 2):
+        return "February"
+    elif (month == 3):
+        return "March"
+    elif (month == 4):
+        return "April"
+    elif (month == 5):
+        return "May"
+    elif (month == 6):
+        return "June"
+    elif (month == 7):
+        return "July"
+    elif (month == 8):
+        return "August"
+    elif (month == 9):
+        return "September"
+    elif (month == 10):
+        return "October"
+    elif (month == 11):
+        return "November"
+    elif (month == 12):
+        return "December"
--- modules/roulette/roulette.py
+++ modules/roulette/roulette.py
@@ -7,12 +7,13 @@ from giphy_client.rest import ApiException
 from random import seed, randint
 import re
 
-from modules.roulette.helper_functions import query_animemanga, generate_random_index 
+from modules.roulette.helper_functions import query_anilist, generate_random_index, monthToString
 from modules.roulette.variables import default_anime, default_manga
 
 load_dotenv()
 giphy_token = os.getenv('GIPHY_TOKEN')
 giphy_instance = giphy_client.DefaultApi()
+
 
 class Roulette(commands.Cog):
     def __init__(self, bot):
@@ -21,24 +22,21 @@ class Roulette(commands.Cog):
     def gif(self, search_term: str, author):
         try:
             if search_term is None:
-                search_term = "nothing"
-
-            api_response = giphy_instance.gifs_search_get(giphy_token, search_term)
+                return "Enter a search term after gif"
+            api_response = giphy_instance.gifs_search_get(
+                giphy_token, search_term)
             data_set = api_response.data
             data_len = len(data_set)
             range = 30
-
-            if(data_len < 30):
+            if (data_len < 30):
                 range = data_len
-
             seed(None)
             rand_num = randint(0, range - 1)
             return author + " Here's a random " + search_term + " gif\n" f'{data_len[rand_num].images.fixed_height.url}'
-
         except ApiException as apie:
             print("Exception when calling DefaultApi")
-        
-    def anime_manga(self, media_type, search_term):
+
+    def anime_manga(self, media_type):
         if media_type == "ANIME":
             array = default_anime
             episode_type = 'episodes'
@@ -49,49 +47,84 @@ class Roulette(commands.Cog):
             episode_type = 'chapters'
             msg_episode_type = 'Chapter Count: '
             embed_colour = 0x2367b0
+        elif media_type == "CHARACTER":
+            array = default_manga
+            embed_colour = 0x2367b0
 
         seed(None)
         rand_num = randint(0, 17000)
 
-        received = query_animemanga(rand_num, media_type)
+        received = query_anilist(rand_num, media_type)
 
-        # Original query failed, query for one of the anime/manga from default array so that 
+        # Original query failed, query for one of the anime/manga from default array so that
         # an anime/manga can be dispalyed
         if received == False:
             default = generate_random_index(array)
-            received = query_animemanga(default, media_type)
+            received = query_anilist(default, media_type)
 
-        anime_obj = received['data']['Media']
+        if media_type == "ANIME" or media_type == "MANGA":
+            anime_obj = received['data']['Media']
+        elif media_type == "CHARACTER":
+            anime_obj = received['data']['Character']
 
-        # Assign the queried attributes to new variables to transform the data and avoid null values 
-        title_english = anime_obj['title']['english'] if anime_obj['title']['english'] is not None else ""
-        title_romaji = anime_obj['title']['romaji'] if anime_obj['title']['romaji'] is not None else ""
-        average_score = str(anime_obj['averageScore']) + "%" if anime_obj['averageScore'] is not None else "N/A"
-        episode_count = str(anime_obj[episode_type]) if ( anime_obj[episode_type] is not None ) else "N/A"
-        anime_description = anime_obj['description'] if ( anime_obj['description'] is not None ) else "N/A"
-        cover_image = anime_obj['coverImage']['large'] if ( anime_obj['coverImage']['large'] is not None ) else ""
-        genres = anime_obj['genres'] if ( anime_obj['genres'] is not None ) else "N/A"
+        title = ""
+        image = ""
+        description = ""
 
-        # Build the embed object for displaying the anime/manga data
+        if (media_type == "ANIME" or media_type == "MANGA"):
+            # Assign the queried attributes to new variables to transform the data and avoid null values
+            title_english = anime_obj['title']['english'] if anime_obj['title']['english'] is not None else ""
+            title_romaji = anime_obj['title']['romaji'] if anime_obj['title']['romaji'] is not None else ""
+            average_score = str(
+                anime_obj['averageScore']) + "%" if anime_obj['averageScore'] is not None else "N/A"
+            episode_count = str(anime_obj[episode_type]) if (
+                anime_obj[episode_type] is not None) else "N/A"
+            anime_description = anime_obj['description'] if (
+                anime_obj['description'] is not None) else "N/A"
+            image = anime_obj['coverImage']['large'] if (
+                anime_obj['coverImage']['large'] is not None) else ""
+            genres = anime_obj['genres'] if (
+                anime_obj['genres'] is not None) else "N/A"
+
+            title = title_english + \
+                " (" + title_romaji + \
+                ")" if title_english != "" and title_english != title_romaji else title_romaji
+            description = "Average Score: " + average_score + "\n" + msg_episode_type + episode_count + "\nGenres: " + \
+                ', '.join(genres) + "\n\nDescription:\n" + \
+                re.sub("</*[a-z]+/*>*|href=", "", anime_description)
+
+        elif (media_type == "CHARACTER"):
+            title = anime_obj['name']['full'] if anime_obj['name']['full'] is not None else ""
+            age = anime_obj['age'] if anime_obj['age'] is not None else "Unknown"
+            birthday_year = anime_obj['dateOfBirth']['year'] if anime_obj['dateOfBirth']['year'] is not None else ""
+            birthday_month = anime_obj['dateOfBirth']['month'] if anime_obj['dateOfBirth']['month'] is not None else ""
+            birthday_day = anime_obj['dateOfBirth']['day'] if anime_obj['dateOfBirth']['day'] is not None else ""
+            gender = anime_obj['gender'] if anime_obj['gender'] is not None else "Unknown"
+            image = anime_obj['image']['large'] if (
+                anime_obj['image']['large'] is not None) else ""
+            char_description = anime_obj['description'] if (
+                anime_obj['description'] is not None) else ""
+            description = "Gender: " + gender + "\nBirthday: " + str(monthToString(birthday_month)) + " " + str(
+                birthday_day) + " " + str(birthday_year) + "\nAge: " + age + "\n\nDescription:\n" + char_description
+
+        # Build the embed object for displaying the data in discord
         embed = discord.Embed(
-            title = title_english + " (" + title_romaji + ")" if title_english != "" and  title_english != title_romaji else title_romaji,  
-            url = "https://anilist.co/" + media_type.lower() + "/" + str(anime_obj['id']),
-            description = "Average Score: " + average_score
-                    + "\n" + msg_episode_type + episode_count 
-                    + "\nGenres: " + ', '.join(genres)
-                    + "\n\nDescription:\n" + re.sub("</*[a-z]+/*>*|href=", "", anime_description),
-            colour = embed_colour 
+            title=title,
+            url="https://anilist.co/" + media_type.lower() + "/" +
+            str(anime_obj['id']),
+            description=description,
+            colour=embed_colour
         )
-        embed.set_thumbnail(url = cover_image)
+        embed.set_thumbnail(url=image)
         return embed
 
     @commands.command()
     async def roulette(self, ctx, media_type: str, search_term: str = ""):
         content = ""
         media_type = media_type.upper()
-        if media_type == "ANIME" or media_type == "MANGA":
-            content = self.anime_manga(media_type, search_term)
-            await ctx.send(embed = content)
+        if media_type == "ANIME" or media_type == "MANGA" or media_type == "CHARACTER":
+            content = self.anime_manga(media_type)
+            await ctx.send(embed=content)
         elif media_type == "GIF":
             content = self.gif(search_term, f'{ctx.author.mention}')
             await ctx.send(content)
